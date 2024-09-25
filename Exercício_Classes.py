f9c0a3a498a4f61a7ec598eb96722ea3267eef75
Dlima159 <deivisonjob159@gmail.com>
2024-09-25 05:37:47 -0300

    Versão 1

---
+++ Exercício_Classes.py
@@ -0,0 +1,19 @@
+
+class Artista:
+    def __init__(self,nome_do_artista,genero_de_musica,albuns):
+        self.nome_do_artista = nome_do_artista
+        self.genero_de_musica = genero_de_musica
+        self.albuns = albuns
+    
+    def adicionar_albuns(self,nome_album):
+        self.albuns+= nome_album
+
+    def listar_albuns(self):
+        for album in self.albuns:
+            print(f'O artista {self.nome_do_artista} do gênero musical {self.genero_de_musica} possui o seguinte album:{album}')
+
+artista1 = Artista('Metallica','Heavy Metal',['Black Album'])
+artista1.adicionar_albuns(['St. Anger','Kill\'em All'])
+artista1.listar_albuns()
+artista1.adicionar_albuns(['And Justice for All...'])
+artista1.listar_albuns()
